335467a89fc384141c70ba07274a542ebdb5ba7b
min_jjiny <kim003512@naver.com>
2020-04-23 20:10:01 +0900

    commit check

--- app/src/main/java/com/example/kill_an/sign/SignInActivity.kt
+++ app/src/main/java/com/example/kill_an/sign/SignInActivity.kt
@@ -9,5 +9,7 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        //민진 커밋 확인
     }
 }
