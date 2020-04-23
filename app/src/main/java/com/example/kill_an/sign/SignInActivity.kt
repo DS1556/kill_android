3610894a36612c3cacde4c7ae8040dda832f9450
ksb0511 <soobin796@gmail.com>
2020-04-23 20:04:36 +0900

    환경 설정 완료

---
+++ app/src/main/java/com/example/kill_an/sign/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kill_an.sign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kill_an.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
--- app/src/main/java/com/example/kill_an/sign/SignUpActivity.kt
+++ app/src/main/java/com/example/kill_an/sign/SignUpActivity.kt
@@ -1,12 +1,13 @@
-package com.example.kill_an
+package com.example.kill_an.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kill_an.R
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
