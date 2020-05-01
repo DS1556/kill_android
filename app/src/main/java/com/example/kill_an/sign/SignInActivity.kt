55789f7a2378c05eb8847d6eaed118dde04cc75a
min_jjiny <kim003512@naver.com>
2020-05-02 00:16:44 +0900

    로그인view, 인텐트 연결

--- app/src/main/java/com/example/kill_an/sign/SignInActivity.kt
+++ app/src/main/java/com/example/kill_an/sign/SignInActivity.kt
@@ -1,8 +1,12 @@
 package com.example.kill_an.sign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kill_an.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_in.view.*
+import kotlinx.android.synthetic.main.activity_sign_in.view.btn_signin_signup
 
 class SignInActivity : AppCompatActivity() {
 
@@ -10,7 +14,9 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        //민진 커밋 확인
-
+        btn_signin_signup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent);
+        }
     }
 }
